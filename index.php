3cc03d0fbd0a2bb7c518668cfb069cb1df122f4b
Mehedee Zaman <devmhd@gmail.com>
2015-06-13 11:36:21 +0600

    DB connect attempt

--- index.php
+++ index.php
@@ -2,5 +2,14 @@
 
 	echo "Hello Heroku!";
 
+	$pg_connection_string = "dbname=dc95fs2dc1uq5r host=ec2-54-83-17-8.compute-1.amazonaws.com port=5432 user=apvuqaquwggarx password=NVjMlNLyjpPe3QQeBdMApaxcZA sslmode=require";
+
+	 $db = pg_connect($pg_connection_string);
+	if (!$db) {
+	    echo "Database connection error."
+	    exit;
+	}
+	 
+	$result = pg_query($db, "CREATE TABLE Student(id int,name varchar(255),address varchar(255),sex varchar(1));");
 
 ?>
